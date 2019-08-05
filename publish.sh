7c7afea3463aa7f903cfc438ad8c2e51d332fdd2
tristanlatr <tris.la.tr@gmail.com>
2019-08-05 10:02:49 -0400

    docs genration is done into publish script

--- publish.sh
+++ publish.sh
@@ -1,4 +1,20 @@
+#!/bin/bash
+
+#Publish on the python index
 python3 setup.py build check sdist bdist_wheel
 twine upload --verbose dist/*
 python3 setup.py clean
 sudo pip3 install msiempy --upgrade
+
+#Generate and publish the documentation
+git clone https://github.com/mfesiem/mfesiem.github.io
+pdoc msiempy --output-dir ./mfesiem.github.io/docs --html --force
+pyreverse -s 1 -f PUB_ONLY -o png -m y msiempy
+mv ./classes.png ./mfesiem.github.io/docs/msiempy
+mv ./packages.png ./mfesiem.github.io/docs/msiempy
+cd mfesiem.github.io
+git add .
+git commit -m 'Generate docs'
+git push origin master
+cd ..
+rm -rf mfesiem.github.io
