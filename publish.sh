cb60b7d92c6e9758f4c8a7c6a364550ae0d3f5fd
tristanlatr <tris.la.tr@gmail.com>
2019-10-03 09:49:54 -0400

    Generate diagrams Thu Oct  3 09:49:54 EDT 2019

--- publish.sh
+++ publish.sh
@@ -1,48 +1,11 @@
 #!/bin/bash
-### CODE COPIED ###
-# Created by Jamie Cruwys on 21/02/2014.
-symbol="#"
-paddingSymbol=" "
-lineLength=120
-charsToOption=1
-charsToName=3
-function generatePadding() {
-    string="";
-    for (( i=0; i < $2; i++ )); do
-        string+="$1";
-    done
-    echo "$string";
-}
-remainingLength=$(( $lineLength - 2 ));
-line=$(generatePadding "${symbol}" "${lineLength}");
-toOptionPadding=$(generatePadding "${paddingSymbol}" "${charsToOption}");
-toNamePadding=$(generatePadding "$paddingSymbol" "$charsToName");
-function generateText() {
-    totalCharsToPad=$((remainingLength - ${#1}));
-    charsToPadEachSide=$((totalCharsToPad / 2));
-    padding=$(generatePadding "$paddingSymbol" "$charsToPadEachSide");
-    totalChars=$(( ${#symbol} + ${#padding} + ${#1} + ${#padding} + ${#symbol} ));
-    if [[ ${totalChars} < ${lineLength} ]]; then
-        echo "${symbol}${padding}${1}${padding}${paddingSymbol}${symbol}";
-    else
-        echo "${symbol}${padding}${1}${padding}${symbol}";
-    fi
-}
-function generateSubTitle() {  
-    echo "$line"
-    generateText "$1"
-    echo "$line"
-}
-### END CODE COPIED ###
-
-#Setting bash strict mode. See http://redsymbol.net/articles/unofficial-bash-strict-mode/
-#set -euo pipefail
+#Not setting bash strict mode. See http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
 IFS=$'\n\t,'
 
 #FUNCTIONS
 usage(){
-    generateSubTitle "Usage"
-    echo "Usage: $0 [-h] [-t] [-p] [-d]" 1>&2
+    echo "[USAGE] $0 [-h] [-t] [-d] <Folder> [-p]" 1>&2
     echo
     echo "This little script is designed to help us manage msiempy."
     echo "Arguments should be passed to the script in the same order they are listed in this"
@@ -50,64 +13,70 @@ usage(){
     echo
     echo -e "\t-h\t\tPrint this help message."
     echo -e "\t-t\t\tLaunch tests, save output to ~/static/tests.txt and push changes to current remote branch IF SUCCESS !."
-    echo -e "\t-p\t\tPublish to PYPI."
     echo -e "\t-d <Folder>\t\tPublish the docs to https://mfesiem.github.io/docs/{folder}/msiempy/"
+    echo -e "\t-p\t\tPublish to PYPI."
     echo -e "\t\tuse 'master' keyword to publish to https://mfesiem.github.io/docs/msiempy/"
     exit -1
 }
 
-while getopts ":htpd:" arg; do
+while getopts ":htd:p" arg; do
     case "${arg}" in
         h) #Print help
             usage
             ;;
         t) #Run tests
-            generateSubTitle "Testing"
+            echo "[BEGIN] Testing"
             python3 setup.py test | tee ~/static/tests.txt
+            echo "[RETURN CODE] $?"
             git add ~/static/tests.txt
+            echo "[RETURN CODE] $?"
             git commit -m "Generate tests $(date)"
+            echo "[RETURN CODE] $?"
             git push
+            echo "[RETURN CODE] $?"
             #echo "[IMPORTANT] Tests pushed"
             ;;
 
-        p) ##Publish on the python index
-            generateSubTitle "PyPI upload"x
-            rm -rf dist
-            python3 setup.py build check sdist bdist_wheel
-            echo 'Dont forget to bump __version__'
-            echo 'Hit ctrl+C to cancel PyPI upload'
-            twine upload --verbose dist/*
-            python3 setup.py clean
-            ;;
+        
 
         d) #Docs
-            generateSubTitle "Documentation"
+            echo "[BEGIN] Documentation"
 
             #Gen diagrams and  :
             pyreverse -s 1 -f PUB_ONLY -o png -m y msiempy
+            echo "[RETURN CODE] $?"
 
             #Generate and publish the documentation
             if [[ ! -d mfesiem.github.io ]]; then
                 git clone https://github.com/mfesiem/mfesiem.github.io
+                echo "[RETURN CODE] $?"
             fi
 
             #mfesiem.github.io
             cd mfesiem.github.io
+            echo "[RETURN CODE] $?"
             git pull -v
+            echo "[RETURN CODE] $?"
             folder=${OPTARG}
             if [ "$folder" = "master" ]; then
                 pdoc msiempy --output-dir ./docs --html --force
+                echo "[RETURN CODE] $?"
                 cp ../classes.png ./docs/msiempy
+                echo "[RETURN CODE] $?"
                 cp ../packages.png ./docs/msiempy
             else
                 pdoc msiempy --output-dir ./docs/${folder} --html --force
+                echo "[RETURN CODE] $?"
                 cp ../classes.png ./docs/${folder}/msiempy
+                echo "[RETURN CODE] $?"
                 cp ../packages.png ./docs/${folder}/msiempy
             fi
 
             git add .
             git commit -m "Generate ${folder} docs $(date)"
+            echo "[RETURN CODE] $?"
             git push origin master
+            echo "[RETURN CODE] $?"
 
             cd ..
 
@@ -115,7 +84,9 @@ while getopts ":htpd:" arg; do
             #push to current branch
             git add .
             git commit -m "Generate diagrams $(date)"
+            echo "[RETURN CODE] $?"
             git push
+            echo "[RETURN CODE] $?"
             
             if [ "$folder" = "master" ]; then
                 url="https://mfesiem.github.io/docs/msiempy/"
@@ -125,9 +96,21 @@ while getopts ":htpd:" arg; do
             
             echo "[IMPORTANT] Documentation on line at : ${url}"
             ;;
+
+        p) ##Publish on the python index
+            echo "[BEGIN] PyPI upload"x
+            rm -rf dist
+            python3 setup.py build check sdist bdist_wheel
+            echo "[RETURN CODE] $?"
+            echo 'Dont forget to bump __version__'
+            echo 'Hit ctrl+C to cancel PyPI upload'
+            twine upload --verbose dist/*
+            echo "[RETURN CODE] $?"
+            python3 setup.py clean
+            ;;
+
         *)
-            generateSubTitle "Syntax mistake"
-            echo "[ERROR] You made a syntax mistake calling the script."
+            echo "[ERROR] Syntax mistake calling the script."
             usage
             exit
             ;;
