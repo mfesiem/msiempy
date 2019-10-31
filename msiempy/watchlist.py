a95d058544001d313c52833f68d033de1add3796
Andy Walden <aw@krakencodes.com>
2019-10-31 14:08:08 -0400

    Refactored watchlist.load_values to use get_internal_file

--- msiempy/watchlist.py
+++ msiempy/watchlist.py
@@ -164,26 +164,3 @@ class Watchlist(NitroDict):
             print('ESM Error: Is watchlist valid?')
         data = self.nitro.get_internal_file(file)
         self.data['values'] = ''.join(data).split('\n')
-
-        # pos = 0
-        # nbytes = 0
-        # resp = self.nitro.request('get_rfile2', ftoken=file, pos=pos, nbytes=nbytes)
-
-        # if resp['FSIZE'] == resp['BREAD']:
-        #     self.data['values'] = resp['DATA'].split('\n')
-        #     self.nitro.request('del_rfile', ftoken=file)
-        #     return
-
-        # data = []
-        # data.append(resp['DATA'])
-        # file_size = int(resp['FSIZE'])
-        # collected = int(resp['BREAD'])
-
-        # while file_size > collected:
-        #     pos += int(resp['BREAD'])
-        #     nbytes = file_size - collected
-        #     resp = self.nitro.request('get_rfile2', ftoken=file, pos=pos, nbytes=nbytes)
-        #     collected += int(resp['BREAD'])
-        #     data.append(resp['DATA'])
-        # self.data['values'] = ''.join(data).split('\n')
-        # resp = self.nitro.request('del_rfile', ftoken=file)
