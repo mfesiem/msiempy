83da001a4f741b7a9665e7e9addb7bc6974cef7e
tristanlatr <tris.la.tr@gmail.com>
2019-11-01 09:40:02 -0400

    better publish script not to forget about documentation

--- publish.sh
+++ publish.sh
@@ -3,41 +3,38 @@
 set -euo pipefail
 IFS=$'\n\t,'
 
-#FUNCTIONS
 usage(){
-    echo "[USAGE] $0 [-h] [-t] [-d] <Folder> [-p]" 1>&2
+    echo "[USAGE] $0 [-h] [-p] <test/master/other>" 1>&2
     echo
-    echo "This little script is designed to help us manage msiempy."
+    echo "Help us publish msiempy on the internet."
     echo
-    echo -e "\t-h\tHelp\tPrint this help message."
-    echo -e "\t-t\tTests\tLaunch tests, save output to ~/static/tests.txt and push changes to current remote branch IF SUCCESS !."
-    echo -e "\t-d <folder> Docs \tPublish the docs to https://mfesiem.github.io/docs/{folder}/msiempy/"
-    echo -e "\t\tuse 'master' keyword to publish to https://mfesiem.github.io/docs/msiempy/"
-    echo -e "\t-p\tPyPI\tPublish to PyPI."
-    
+    echo -e "\t-h\tPrint this help message."
+    echo -e "\t-p\t<keyword>\tGenerate documentation and publish to PyPi."
+    echo -e "\t\tuse 'master' keyword to publish to production PyPi and docs to https://mfesiem.github.io/docs/msiempy/"
+    echo -e "\t\tuse 'test' keyword to publish to test PyPi and docs to https://mfesiem.github.io/docs/test/msiempy/"
+    echo -e "\t\tuse whatever other keyword to publish only the docs to https://mfesiem.github.io/docs/{keyword}/msiempy/"
     echo 
-    echo -e "\tExample : $0 -d dev -p"
-    echo -e "\tGenerate the documentation to https://mfesiem.github.io/docs/{folder}/msiempy/"
-    echo -e "\tand publish the module on PyPI."
+    echo -e "\tExample : $0 -p test"
+    echo -e "\t\tGenerate the documentation to https://mfesiem.github.io/docs/test/msiempy/"
+    echo -e "\t\tand publish the module on test PyPI."
+    echo
+    echo -e "\tExample : $0 -p master"
+    echo -e "\t\tGenerate the documentation to https://mfesiem.github.io/docs//msiempy/"
+    echo -e "\t\tand publish the module on PyPI."
+    echo
+    echo -e "\tExample : $0 -p dev"
+    echo -e "\t\tOnly generate the documentation to https://mfesiem.github.io/docs/dev/msiempy/"
     exit -1
 }
 
-while getopts ":htd:p" arg; do
+while getopts ":hp:" arg; do
     case "${arg}" in
         h) #Print help
             usage
             ;;
-        t) #Run tests
-            echo "[BEGIN] Testing"
-            echo "[RUNNING] python3 setup.py test | tee ~/static/tests.txt"
-            python3 setup.py test | tee ~/static/tests.txt
-            echo "[RUNNING] git add ~/static/tests.txt && git commit -m "Generate tests $(date)" && git push"
-            git add ~/static/tests.txt && git commit -m "Generate tests $(date)" && git push
-            echo "[SUCCESS] Tests results pushed"
-            ;;
 
-        d) #Docs
-            echo "[BEGIN] Documentation"
+        p) ##Publish on the python index
+            echo "[BEGIN] Documentation upload"
 
             #Gen diagrams and  :
             echo "[RUNNING] pyreverse -s 1 -f PUB_ONLY -o png -m y msiempy"
@@ -52,46 +49,53 @@ while getopts ":htd:p" arg; do
                 cd mfesiem.github.io && git pull -v && cd ..
             fi
 
-            folder=${OPTARG}
-            if [ "$folder" = "master" ]; then
+            keyword=${OPTARG}
+            if [ "$keyword" = "master" ]; then
                 echo "[RUNNING] pdoc3 msiempy --output-dir ./mfesiem.github.io/docs --html --force"
                 rm -rf ./mfesiem.github.io/docs/msiempy/
                 pdoc3 msiempy --output-dir ./mfesiem.github.io/docs --html --force
                 mv ./classes.png ./mfesiem.github.io/docs/msiempy
                 mv ./packages.png ./mfesiem.github.io/docs/msiempy
             else
-                echo "[RUNNING] pdoc3 msiempy --output-dir ./mfesiem.github.io/docs/${folder} --html --force"
-                rm -rf ./mfesiem.github.io/docs/${folder}/msiempy
-                pdoc3 msiempy --output-dir ./mfesiem.github.io/docs/${folder} --html --force
-                mv ./classes.png ./mfesiem.github.io/docs/${folder}/msiempy
-                mv ./packages.png ./mfesiem.github.io/docs/${folder}/msiempy
+                echo "[RUNNING] pdoc3 msiempy --output-dir ./mfesiem.github.io/docs/${keyword} --html --force"
+                rm -rf ./mfesiem.github.io/docs/${keyword}/msiempy
+                pdoc3 msiempy --output-dir ./mfesiem.github.io/docs/${keyword} --html --force
+                mv ./classes.png ./mfesiem.github.io/docs/${keyword}/msiempy
+                mv ./packages.png ./mfesiem.github.io/docs/${keyword}/msiempy
             fi
             
-            echo "[RUNNING] cd mfesiem.github.io && git add . && git commit -m \"Generate ${folder} docs $(date)\" && git push origin master && cd .."
-            cd mfesiem.github.io && git add . && git commit -m "Generate ${folder} docs $(date)" && git push origin master && cd ..
+            echo "[RUNNING] cd mfesiem.github.io && git add . && git commit -m \"Generate ${keyword} docs $(date)\" && git push origin master && cd .."
+            cd mfesiem.github.io && git add . && git commit -m "Generate ${keyword} docs $(date)" && git push origin master && cd ..
 
-            if [ "$folder" = "master" ]; then
+            if [ "$keyword" = "master" ]; then
                 url="https://mfesiem.github.io/docs/msiempy/index.html"
             else
-                url="https://mfesiem.github.io/docs/${folder}/msiempy/index.html"
+                url="https://mfesiem.github.io/docs/${keyword}/msiempy/index.html"
             fi
-            
-            echo "[SUCCESS] Documentation on line at : ${url}"
-            ;;
 
-        p) ##Publish on the python index
+            echo "[SUCCESS] Documentation on line at : ${url}"
+
             echo "[BEGIN] PyPI upload"
             rm -rf dist
 
             echo "[RUNNING] python3 setup.py build check sdist bdist_wheel"
             python3 setup.py build check sdist bdist_wheel
-            
-            echo "[RUNNING] twine upload --verbose dist/*"
-            echo 'Dont forget to bump __version__'
-            echo 'Hit ctrl+C to cancel PyPI upload'
-            twine upload --verbose dist/*
-            
-            echo "[SUCCESS] Module published at : https://pypi.org/project/msiempy/"
+
+            if [ "$keyword" = "master" ]; then
+                echo "[RUNNING] twine upload --verbose dist/*"
+                echo 'Dont forget to bump __version__'
+                echo 'Hit ctrl+C to cancel PyPI upload'
+                twine upload --verbose dist/*
+                echo "[SUCCESS] Module published at : https://pypi.org/project/msiempy/"
+            fi
+            if [ "$keyword" = "test" ]; then
+                echo "[RUNNING] twine upload --repository-url https://test.pypi.org/legacy/ dist/*"
+                echo 'Dont forget to bump __version__'
+                echo 'Hit ctrl+C to cancel PyPI upload'
+                twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+                echo "[SUCCESS] Module published at : https://test.pypi.org/project/msiempy/"
+            fi
+
             python3 setup.py clean
             ;;
 
