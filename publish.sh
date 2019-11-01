1a507c74dbed08c5736dc70a91dbd8c2298fa696
tristanlatr <tris.la.tr@gmail.com>
2019-11-01 16:05:52 -0400

    final publish script, to test with master

--- publish.sh
+++ publish.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
-#Not setting bash strict mode. See http://redsymbol.net/articles/unofficial-bash-strict-mode/
+#Setting bash strict mode. See http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 IFS=$'\n\t,'
 
 usage(){
-    echo "[USAGE] $0 [-h] [-p] <test/master/other>" 1>&2
+    echo "[USAGE] $0 [-h] [-p] <test/master>" 1>&2
     echo
     echo "Help us publish msiempy on the internet."
     echo
     echo -e "\t-h\tPrint this help message."
-    echo -e "\t-p\t<keyword>\tGenerate documentation and publish to PyPi."
-    echo -e "\t\tuse 'master' keyword to publish to production PyPi and docs to https://mfesiem.github.io/docs/msiempy/"
-    echo -e "\t\tuse 'test' keyword to publish to test PyPi and docs to https://mfesiem.github.io/docs/test/msiempy/"
-    echo -e "\t\tuse whatever other keyword to publish only the docs to https://mfesiem.github.io/docs/{keyword}/msiempy/"
-    echo 
-    echo -e "\tExample : $0 -p test"
-    echo -e "\t\tGenerate the documentation to https://mfesiem.github.io/docs/test/msiempy/"
-    echo -e "\t\tand publish the module on test PyPI."
+    echo -e "\t-p\t<test/master>\tGit tag, push the technical documentation and publish to PyPi. In this order."
     echo
-    echo -e "\tExample : $0 -p master"
-    echo -e "\t\tGenerate the documentation to https://mfesiem.github.io/docs//msiempy/"
-    echo -e "\t\tand publish the module on PyPI."
+    echo -e "\t\t'test' keyword : "
+    echo -e "\t\t\t- Tag version with '-test' flag"
+    echo -e "\t\t\t- Publish docs to https://mfesiem.github.io/docs/test/msiempy/"
+    echo -e "\t\t\t- Publish module to https://test.pypi.org/project/msiempy/"
     echo
-    echo -e "\tExample : $0 -p dev"
-    echo -e "\t\tOnly generate the documentation to https://mfesiem.github.io/docs/dev/msiempy/"
+    echo -e "\t\t'master' keyword : "
+    echo -e "\t\t\t- Delete '-test' tag and tag version"
+    echo -e "\t\t\t- Publish docs to https://mfesiem.github.io/docs/msiempy/"
+    echo -e "\t\t\t- Publish module to https://pypi.org/project/msiempy/"
+    echo
     exit -1
 }
 
@@ -33,88 +30,79 @@ while getopts ":hp:" arg; do
             usage
             ;;
 
-        p) ##Publish on the python index
-            echo "[BEGIN] Documentation upload"
-
-            #Gen diagrams and  :
-            echo "[RUNNING] pyreverse -s 1 -f PUB_ONLY -o png -m y msiempy"
-            pyreverse -s 1 -f PUB_ONLY -o png -m y msiempy
-
-            #Generate and publish the documentation
-            if [[ ! -d mfesiem.github.io ]]; then
-                echo "[RUNNING] git clone https://github.com/mfesiem/mfesiem.github.io"
-                git clone https://github.com/mfesiem/mfesiem.github.io
-            else
-                echo "[RUNNING] cd mfesiem.github.io && git pull -v && cd .."
-                cd mfesiem.github.io && git pull -v && cd ..
-            fi
-
+        p)
+            # Figuring version
+            version=`grep __version__ ./msiempy/__version__.py | cut -d "'" -f 2`
+        
+            # Checking keyword
             keyword=${OPTARG}
-            if [ "$keyword" = "master" ]; then
-                echo "[RUNNING] pdoc3 msiempy --output-dir ./mfesiem.github.io/docs --html --force"
-                rm -rf ./mfesiem.github.io/docs/msiempy/
-                pdoc3 msiempy --output-dir ./mfesiem.github.io/docs --html --force
-                mv ./classes.png ./mfesiem.github.io/docs/msiempy
-                mv ./packages.png ./mfesiem.github.io/docs/msiempy
-            else
-                echo "[RUNNING] pdoc3 msiempy --output-dir ./mfesiem.github.io/docs/${keyword} --html --force"
-                rm -rf ./mfesiem.github.io/docs/${keyword}/msiempy
-                pdoc3 msiempy --output-dir ./mfesiem.github.io/docs/${keyword} --html --force
-                mv ./classes.png ./mfesiem.github.io/docs/${keyword}/msiempy
-                mv ./packages.png ./mfesiem.github.io/docs/${keyword}/msiempy
-            fi
-            
-            echo "[RUNNING] cd mfesiem.github.io && git add . && git commit -m \"Generate ${keyword} docs $(date)\" && git push origin master && cd .."
-            cd mfesiem.github.io && git add . && git commit -m "Generate ${keyword} docs $(date)" && git push origin master
-            cd ..
 
             if [ "$keyword" = "master" ]; then
-                url="https://mfesiem.github.io/docs/msiempy/index.html"
-            else
-                url="https://mfesiem.github.io/docs/${keyword}/msiempy/index.html"
-            fi
-
-            echo "[SUCCESS] Documentation on line at : ${url}"
-
-            echo "[BEGIN] PyPI upload"
-            rm -rf dist
-
-            echo "[RUNNING] python3 setup.py build check sdist bdist_wheel"
-            python3 setup.py build check sdist bdist_wheel
-            
-            echo '[INFO] Dont forget to run tests with ./setup.py test'
-            echo '[INFO] And dont forget to bump __version__'
-            echo '[INFO] Hit ctrl+C to cancel PyPI upload'
-
-            if [ "$keyword" = "master" ]; then
-                version=`grep __version__ ./msiempy/__version__.py | cut -d "'" -f 2`
-
+                docs_folder="mfesiem.github.io/docs"
+                repository_url="https://pypi.org/"
+                # Deleting '-test' tag
                 if [ -n `git tag -l "${version}-test"` ]; then
                     echo "[RUNNING] git tag -d ${version}-test && git push origin --delete ${version}-test"
                     git tag -d ${version}-test && git push origin --delete ${version}-test
                 fi
 
+                # Tag
                 echo "[RUNNING] git tag -a ${version} -m "Version ${version}" && git push --tags"
                 git tag -a ${version} -m "Version ${version}" && git push --tags
 
-                echo "[RUNNING] twine upload --verbose dist/*"
-                twine upload --verbose dist/*
-                echo "[SUCCESS] Module published at : https://pypi.org/project/msiempy/"
+            else
+                if [ "$keyword" = "test" ]; then
+                    docs_folder="mfesiem.github.io/docs/test"
+                    repository_url="https://test.pypi.org"
+
+                    # Test tag
+                    echo "[RUNNING] git tag -a ${version}-test -m "Version ${version}-test" && git push --tags"
+                    git tag -a ${version}-test -m "Version ${version}-test" && git push --tags
+                
+                else
+                    echo "[ERROR] The keyword must be 'test' or 'master'"
+                    exit -1
+                fi
             fi
-            if [ "$keyword" = "test" ]; then
-                version=`grep __version__ ./msiempy/__version__.py | cut -d "'" -f 2`
-                echo "[RUNNING] git tag -a ${version}-test -m "Version ${version}-test" && git push --tags"
-                git tag -a ${version}-test -m "Version ${version}-test" && git push --tags
 
-                echo "[RUNNING] twine upload --repository-url https://test.pypi.org/legacy/ dist/*"
-                twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-                echo "[SUCCESS] Module published at : https://test.pypi.org/project/msiempy/"
+            # Generating diagrams
+            echo "[RUNNING] pyreverse -s 1 -f PUB_ONLY -o png -m y msiempy"
+            pyreverse -s 1 -f PUB_ONLY -o png -m y msiempy
+
+            # Cloning or pulling changes from the documentation
+            if [[ ! -d mfesiem.github.io ]]; then
+                echo "[RUNNING] git clone https://github.com/mfesiem/mfesiem.github.io"
+                git clone https://github.com/mfesiem/mfesiem.github.io
+            else
+                echo "[RUNNING] cd mfesiem.github.io && git pull -v && cd .."
+                cd mfesiem.github.io && git pull --quiet && cd ..
             fi
+            
+            # Generating documentation
+            echo "[RUNNING] pdoc3 msiempy --output-dir ./mfesiem.github.io/docs --html --force"
+            rm -rf ./${docs_folder}/msiempy/
+            pdoc3 msiempy --output-dir ./${docs_folder} --html --force
+            mv ./classes.png ./${docs_folder}/msiempy
+            mv ./packages.png ./${docs_folder}/msiempy
+            
+            # Pushing docs
+            echo "[RUNNING] cd mfesiem.github.io && git add . && git commit -m \"Generate ${keyword} docs $(date)\" && git push origin master"
+            cd mfesiem.github.io && git add . && git commit -m "Generate ${keyword} docs $(date)" --quiet && git push origin master --quiet
+            cd ..
+            
+            echo "[SUCCESS] Documentation at : https://${docs_folder}/msiempy/"
 
-            echo "[SUCCESS] Documentation on line at : ${url}"
+            # Building module
+            echo "[RUNNING] python3 setup.py build check sdist bdist_wheel"
+            rm -rf dist
+            python3 setup.py --quiet build check sdist bdist_wheel
+            
+            # Publish to PyPi
+            echo "[RUNNING] twine upload --repository-url ${repository_url} dist/*"
+            twine upload --repository-url ${repository_url}/legacy/ dist/*
+            python3 setup.py --quiet clean
 
-            python3 setup.py clean
-
+            echo "[SUCCESS] Module published at : https://${docs_folder}/msiempy/"
             ;;
 
         *)
@@ -125,4 +113,4 @@ while getopts ":hp:" arg; do
     esac
 done
 shift $((OPTIND-1))
-echo "[END] usage: $0 [-h] [-p <keyword>]"
+echo "[END] usage: $0 [-h] [-p <test/master>]"
