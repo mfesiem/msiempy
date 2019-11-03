b63cf1b23979a3198ab86655361997575aa5662f
Andy Walden <aw@krakencodes.com>
2019-11-03 18:55:18 -0500

    Commented out text proxy that should have been commented before

--- msiempy/__init__.py
+++ msiempy/__init__.py
@@ -690,7 +690,7 @@ class NitroSession():
                 verify=self.config.ssl_verify,
                 timeout=self.config.timeout,
                 # Uncomment for debugging.
-                proxies={"http": "http://127.0.0.1:8888", "https":"http:127.0.0.1:8888"}
+                #proxies={"http": "http://127.0.0.1:8888", "https":"http:127.0.0.1:8888"}
             )
 
             if raw :
