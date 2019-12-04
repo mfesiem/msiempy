f5a0ebb75bfd8d1f018b25e4545be2cd97d4aab7
tristanlatr <tris.la.tr@gmail.com>
2019-12-04 02:56:49 -0500

    added direct esm config

--- samples/msiempy_setup.py
+++ samples/msiempy_setup.py
@@ -1,4 +1,20 @@
 from msiempy import NitroConfig
+from msiempy.__utils__ import tob64
+import argparse
+
+parser = argparse.ArgumentParser(description="""Setup authentication""")
+parser.add_argument('--host', metavar='ESM address')    
+parser.add_argument('--user', metavar='username')
+parser.add_argument('--passwd', metavar='passwd')
+args= parser.parse_args()
+
 config=NitroConfig()
-config.iset('esm')
+
+if args.host and args.user and args.passwd : 
+    config.set('esm','host', args.host)
+    config.set('esm', 'user', args.user)
+    config.set('esm', 'passwd', tob64(args.passwd))
+else :
+    config.iset('esm')
+
 config.write()
