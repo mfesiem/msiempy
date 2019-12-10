30c3bf397840ab6b6e0ca5a4bba9f01437c6ea50
tristanlatr <19967168+tristanlatr@users.noreply.github.com>
2019-12-09 21:11:28 -0500

    Update msiempy_setup.py

--- samples/msiempy_setup.py
+++ samples/msiempy_setup.py
@@ -3,15 +3,14 @@ from msiempy.__utils__ import tob64
 import argparse
 """
 Usage example:  
-
-    $ python ./samples/msiempy_setup.py --set esm host 207.179.200.58:4443 --set esm user NGCP --set esm passwd ${{ secrets.esmpass }} --set general verbose true --set general logfile ./log.txt --set general timeout 60
-
-
-    $ python ./samples/msiempy_setup.py  
-    Enter [esm]host. Press <Enter> to keep empty: <type here>  
+ 1. 
+    $ python samples/msiempy_setup.py  
+    Enter [esm]host. Press <Enter> to keep empty: <type here>   
     Enter [esm]user. Press <Enter> to keep empty: <type here>  
     Enter [esm]passwd. Press <Enter> to skip: <type here>  
-
+ 2. You can pass all config values as a list of (( <section> <option> <value> )) to the --set argument repetitively. 
+    $ python samples/msiempy_setup.py --set esm host 207.179.200.58:4443 --set esm user NGCP --set esm passwd 'myp@assw0rd'  
+    $ python samples/msiempy_setup.py --set general verbose true --set general logfile ./log.txt --set general timeout 60  
 """
 
 parser = argparse.ArgumentParser(description="""Setup msiempy configuration.""")
@@ -19,7 +18,6 @@ parser.add_argument('--set', '-s', metavar="'<section>' '<option>' '<value>'", a
 args= parser.parse_args()
 
 config=NitroConfig()
-print(args)
 
 if len(args.set)>0:
     for setting in args.set:
