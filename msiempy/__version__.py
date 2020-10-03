ab080ad06832d4431a87efa58a61fbc24a6e1cf4
tristanlatr <tris.la.tr@gmail.com>
2020-10-03 13:40:22 -0400

    Removed arguments from find_packages and bump version

--- msiempy/__version__.py
+++ msiempy/__version__.py
@@ -1,10 +1,10 @@
 """
 Project version and meta informations.
 """
-__version__ = "0.3.3"
+__version__ = "0.3.4.dev0"
 __title__ = 'msiempy'
 __description__ = 'msiempy - McAfee SIEM API Python wrapper'
-__author__ = 'andywalden, tristanlatr, mathieubeland, and other contributors. '
+__author__ = 'andywalden, tristanlatr, mathieubeland, and other contributors'
 __author_email__ = ''
 __license__ = 'The MIT License'
 __url__ = 'https://github.com/mfesiem/msiempy'
