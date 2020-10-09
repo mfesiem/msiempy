0b05adc1f90cf927ea6cafed1df65def680aebdf
tristanlatr <tris.la.tr@gmail.com>
2020-10-09 09:07:47 -0400

    Fixing Table of content

--- msiempy/__init__.py
+++ msiempy/__init__.py
@@ -8,23 +8,26 @@ Looking for a CLI tool? Checkout the `msiem CLI<https://github.com/mfesiem/msiem
 Already know what you're looking for? Checkout the Class Hierarchy.    
 
 Quick links: 
-- `GitHub<https://github.com/mfesiem/msiempy>`_, 
-- `PyPI<https://pypi.org/project/msiempy/>`_, 
-- `Class diagram<https://mfesiem.github.io/docs/msiempy/classes.png>`_, 
-- `Packages diagram<https://mfesiem.github.io/docs/msiempy/packages.png>`_,
+
+- `GitHub<https://github.com/mfesiem/msiempy>`_ 
+- `PyPI<https://pypi.org/project/msiempy/>`_ 
+- `Class diagram<https://mfesiem.github.io/docs/msiempy/classes.png>`_ 
+- `Packages diagram<https://mfesiem.github.io/docs/msiempy/packages.png>`_
 - `other SIEM API references<https://mfesiem.github.io>`_ (generated PDFs and other links)  
 
 Table of content:
+
 - `Installation`_
 - `Authentication and configuration setup`_
 - `A few usage exemples`_
-* `Execute an event query`_
-* `Acknowledge alarms`_
-* `Make direct API calls`_
-* `Add a note to events`_
-* `Fetch ESM infos`_
-* `Add a Datasource`_
-* `Add values to a Watchlist`_
+
+        * `Execute an event query`_
+        * `Acknowledge alarms`_
+        * `Make direct API calls`_
+        * `Add a note to events`_
+        * `Fetch ESM infos`_
+        * `Add a Datasource`_
+        * `Add values to a Watchlist`_
 
 
 Installation
