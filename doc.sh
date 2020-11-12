17653ac3fc64182d7b327de917e34e5c3d2bec5c
tristanlatr <tris.la.tr@gmail.com>
2020-11-12 18:47:45 -0500

    Hopefully, this will work

--- doc.sh
+++ doc.sh
@@ -2,16 +2,22 @@
 # This shell script builds the api docs for msiempy 
 # I've been running it from MacOS but I beleive it's valid on Linux too. 
 # Will first write the docs under the version's folder, and then ovwewrites default (lastest). 
-# See the docs files under the ./docs folder 
+# See the docs files under the ./docs folder or the 1rst CLI argument
 # See the mkdocs site under ./site folder
+
+# Requirements: graphviz, pydoctor mkdocs mkdocs-awesome-pages-plugin
+# sudo apt-get install graphviz || sudo yum install graphviz || brew install graphviz
 
 # Stop if errors
 set -euo pipefail
 
-# Install requirements
+# ./docs
+docs=${1:-"./docs"}    
+
+# Install python requirements
 python3 -m pip install pydoctor mkdocs mkdocs-awesome-pages-plugin
 
-docsfolder="./docs/$(python3 setup.py -V)"
+docsfolder="${docs}/$(python3 setup.py -V)"
 mkdir -p "${docsfolder}"
 
 pydoctor \
@@ -24,29 +30,27 @@ pydoctor \
     --intersphinx=https://docs.python.org/3/objects.inv \
     --html-output="${docsfolder}"
 
-# Remove the current PyDoctor files in the top level directory
-rm -f ./docs/*.html
-rm -f ./docs/*.js
-rm -f ./docs/*.css
-rm -f ./docs/*.inv
-
 # Generate diagrams
-sudo apt-get install graphviz || sudo yum install graphviz || brew install graphviz
+
 pyreverse -s 1 -f PUB_ONLY -o png -m y msiempy
+
 mv ./classes.png "${docsfolder}"
 mv ./packages.png "${docsfolder}"
 
 # Hack the mkdocs index to show msiempy version
-echo "# msiempy $(python3 -c 'from msiempy import VERSION; print(VERSION)')" > "${docsfolder}/index.md"
+
+project_version="$(python3 -c 'from msiempy import VERSION; print(VERSION)')"
+echo "# msiempy ${project_version}" > "${docsfolder}/index.md"
 echo "[View Documentation](msiempy.html)" >> "${docsfolder}/index.md"
 echo "" >> "${docsfolder}/index.md"
 echo "[Project Home](https://github.com/mfesiem/msiempy)" >> "${docsfolder}/index.md"
 
-# Copy the docs in the versionned folder to the latest, avoid same name error on unix
+# Copy the docs in the versionned folder to the latest
 if [ "$(uname)" = Linux ]; then
-    find "${docsfolder}/" -type f -print0 | sort -zu | xargs -0 cp -ut "./docs" 
+    cp -rf "${docsfolder}/*" "${docs}"
 else
-    cp -r "${docsfolder}/" "./docs"
+    cp -rf "${docsfolder}/" "${docs}"
 fi
+
 
 mkdocs build
